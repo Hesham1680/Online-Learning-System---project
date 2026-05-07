950a73816a835578a091e2625d4f3e7ec9999b35
Dani George <dgeorges060@gmail.com>
2026-05-07 15:16:25 +0300

    Create Whishlist.h

---
+++ Whishlist.h
@@ -0,0 +1,12 @@
+#ifndef WISHLIST_H
+#define WISHLIST_H
+
+#include "User.h"
+#include "Course.h"
+
+void addToWishlist(User& user, const Course& course);
+void removeFromWishlist(User& user, int courseId);
+void displayWishlist(const User& user);
+bool isInWishlist(const User& user, int courseId);
+
+#endif
