cf6a8c250da7f01b9f3cbca33135576a72b50418
ahmedyasser-7 <ahmad.yasser.0221@gmail.com>
2026-05-07 15:29:05 +0300

    Add Course struct and related function declarations

---
+++ Course.h
@@ -0,0 +1,24 @@
+#ifndef COURSE_H
+#define COURSE_H
+
+#include <string>
+#include <vector>
+
+struct Course {
+    int id;
+    std::string name;
+    std::string description;
+    std::string category;
+    double price;
+    int hours;
+    bool hasCertification;
+    std::string difficulty;
+    std::string instructorName;
+};
+
+std::vector<Course>& getCourseList();
+void displayCourseList(bool subscriberMode);
+void displayCourseDetail(const Course& course, bool subscriberMode);
+void addCourse(const Course& course);
+
+#endif
