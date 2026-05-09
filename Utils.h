7ed579a065c01c94d9d63ef82cd1e6fa8514e3f3
360yassin <75494080+360yassin@users.noreply.github.com>
2026-05-09 22:00:09 +0300

    Rename utility function signatures to PascalCase

--- Utils.h
+++ Utils.h
@@ -1,5 +1,4 @@
-/* 
-* Copyright 2026 Cairo University - SCS253 - Online Learning System
+/* Copyright 2026 Cairo University - SCS253 - Online Learning System
 * Declares shared utility functions for input validation and display.
 */ 
 
@@ -8,9 +7,9 @@
 
 #include <string>
 
-int getValidInt(int min, int max);
-std::string getValidString(const std::string& prompt);
-void printDivider();
-void printHeader(const std::string& title);
+int GetValidInt(int min, int max);
+std::string GetValidString(const std::string& prompt);
+void PrintDivider();
+void PrintHeader(const std::string& title);
 
 #endif
